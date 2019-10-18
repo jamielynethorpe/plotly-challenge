9d01232bd5dee477f1636e478ea48469a663b061
Jamie Lyne Thorpe <jamielynethorpe@gmail.com>
2019-10-18 19:14:48 -0400

    update files

--- static/js/app.js
+++ static/js/app.js
@@ -18,12 +18,73 @@ function buildMetadata(sample) {
       row.text(`${key}: ${value}`);
     });
   });
+    d3.json(url).then(function(sampledata) {
+      console.log(sampledata.WFREQ);
+      level = sampledata.WFREQ
+
+
+      var degrees = ((level)*20-180)*-1;
+      alert(degrees);
+      radius = .5;
+      var radians = degrees * Math.PI / 180;
+      var x = radius * Math.cos(radians);
+      var y = radius * Math.sin(radians);
+
+      // Path: may have to change to create a better triangle
+      var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
+          pathX = String(x),
+          space = ' ',
+          pathY = String(y),
+          pathEnd = ' Z';
+      var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+      var data = [{ type: 'category',
+        x: [0], y:[0],
+          marker: {size: 28, color:'850000'},
+          showlegend: false,
+          name: 'speed',
+          text: level,
+          hoverinfo: 'text+name'
+        },
+        { values: [81,81/9,81/9,81/9,81/9,81/9,81/9,81/9,81/9,81/9],
+          rotation: 90,
+          
+          text: [" ","8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1"],
+          textinfo: 'text',
+          textposition:'inside',      
+          marker: {colors:['rgba(255,255,255,1)','rgba(0,105,11,.5)','rgba(10,120,22,0.5)','rgba(14,127,0,0.5)','rgba(110,154,22,.5)','rgba(170,202,42,.5)','rgba(202,209,95,.5)','rgba(210,206,145,.5)','rgba(232,225,202,.5)','rgba(240,230,215,.5)']},
+          labels: [" ","8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1"],
+          hoverinfo: 'label',
+          hole: .5,
+          type: 'pie',
+          showlegend: false
+        }];
+
+      var layout = {
+        shapes:[{
+            type: 'path',
+            path: path,
+            fillcolor: '850000',
+            line: {
+              color: '850000'
+            }
+          }],
+        title: 'Belly Button Washing Frequency <br> Scrubs per Week',
+        height: 500,
+        width:  500,
+        xaxis: {visible: false, range: [-1, 1]},
+        yaxis: {visible: false, range: [-1, 1]}
+      };
+      Plotly.newPlot('gauge', data, layout);
+
+  });
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
 }
+  
 
 function buildCharts(sample) {
 
@@ -69,11 +130,13 @@ function buildCharts(sample) {
         type: "pie"
       }];
       
+      // var layout = {
+      //   height: 600,
+      //   width: 800
+      // };
       var layout = {
-        height: 600,
-        width: 800
+      // margin: {t:0,l:0}
       };
-      
       Plotly.plot("pie", data, layout);
     });
       
